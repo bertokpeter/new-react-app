a47864f3b300468674f0fd0e032aab1b3fd94a83
bertokpeter <bertokpeter92@gmail.com>
2017-12-01 17:31:46 +0100

    refact(index): practice imports exports

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function coinToss () {
-  // Randomly return either 'heads' or 'tails'.
-  return Math.random() < 0.5 ? 'heads' : 'tails';
-}
-
-const pics = {
-  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
-  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
-};
-
-const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
+import { img } from './img.js'
 
 const judgmental = Math.random() < 0.5;
 
